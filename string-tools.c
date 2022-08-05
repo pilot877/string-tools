b34d717bbafd4df47c83ef83ee1ee748544db73b
pilot877 <90967369+pilot877@users.noreply.github.com>
2022-08-06 00:27:46 +1000

    make string length own function, fix makefile

--- string-tools.c
+++ string-tools.c
@@ -3,12 +3,17 @@
 #include <malloc.h>
 
 char *reverse_string(char *input_string) {
-    int str_len;
-    for (str_len = 0; input_string[str_len] != '\0'; str_len++);
+    int str_len = string_length(input_string);
     char *new_str = malloc(str_len + 1);
     for (int x = str_len; x >= 0; x--) {
         new_str[str_len-x-1] = input_string[x];
     }
     new_str[str_len + 1] = '\0';
     return new_str;
+}
+
+int string_length(char *input_string) {
+    int str_len;
+    for (str_len = 0; input_string[str_len] != '\0'; str_len++);
+    return str_len;
 }
--- string-tools.h
+++ string-tools.h
@@ -1,6 +1,7 @@
-#ifndef REVERSE_STRING_LIBRARY_H
-#define REVERSE_STRING_LIBRARY_H
+#ifndef REVERSE_STRING_STRING_TOOLS_H
+#define REVERSE_STRING_STRING_TOOLS_H
 
 char *reverse_string(char *input_string);
+int string_length(char *input_string);
 
-#endif //REVERSE_STRING_LIBRARY_H
+#endif //REVERSE_STRING_STRING_TOOLS_H
