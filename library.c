29e1f1ca470e9521dacd3468979d80328a4f8322
pilot877 <90967369+pilot877@users.noreply.github.com>
2022-08-05 22:29:23 +1000

    added initial files

---
+++ library.c
@@ -0,0 +1,14 @@
+#include "library.h"
+
+#include <malloc.h>
+
+char *reverse_string(char *input_string) {
+    int str_len;
+    for (str_len = 0; input_string[str_len] != '\0'; str_len++);
+    char *new_str = malloc(str_len + 1);
+    for (int x = str_len; x >= 0; x--) {
+        new_str[str_len-x-1] = input_string[x];
+    }
+    new_str[str_len + 1] = '\0';
+    return new_str;
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef REVERSE_STRING_LIBRARY_H
+#define REVERSE_STRING_LIBRARY_H
+
+char *reverse_string(char *input_string);
+
+#endif //REVERSE_STRING_LIBRARY_H
